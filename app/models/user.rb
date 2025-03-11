47b6254338b1ab76dd2f275c5d0146da9225ea4d
peco-tanaka <ok.computeruk1929@gmail.com>
2025-03-11 23:30:20 +0900

    ユーザー新規投稿機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :nickname, :birth_date, presence: true
 end
