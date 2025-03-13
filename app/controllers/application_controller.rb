1ad6024e57abcdc524d850d713a4b4bac5cea1e3
peco-tanaka <ok.computeruk1929@gmail.com>
2025-03-13 19:07:04 +0900

    不要なページ遷移制御を削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :nickname, :birth_date])
   end
-  def after_sign_in_path_for(resource)
-    root_path
-  end
 end
