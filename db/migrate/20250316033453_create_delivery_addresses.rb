7a3e6d7c49adef0ebac1f7b0fbd305e4f823008a
peco-tanaka <ok.computeruk1929@gmail.com>
2025-03-16 12:47:13 +0900

    マイグレーションファイルにカラムを追加

--- db/migrate/20250316033453_create_delivery_addresses.rb
+++ db/migrate/20250316033453_create_delivery_addresses.rb
@@ -1,7 +1,12 @@
 class CreateDeliveryAddresses < ActiveRecord::Migration[7.1]
   def change
     create_table :delivery_addresses do |t|
-
+      t.string       :postal_code, null: false
+      t.integer      :prefecture_id, null: false
+      t.string       :city, null: false
+      t.string       :address, null: false
+      t.string       :building_name
+      t.string       :phone_number, null: false
       t.timestamps
     end
   end
