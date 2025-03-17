e7e3dcd9c9df0c8c9e50d9b8839010312accffe5
peco-tanaka <ok.computeruk1929@gmail.com>
2025-03-17 19:03:08 +0900

    決済機能の実装。トークン作成から送信まで

---
+++ app/javascript/card.js
@@ -0,0 +1,38 @@
+const pay = () => {
+  // 後で環境変数で設定する
+  const publicKey = "pk_test_ce03814f83a2595d60a7014e"
+  const payjp = Payjp(publicKey);
+
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+        alert(response.error.message);
+      } else {
+        const token = response.id;
+
+        const tokenInput = document.createElement("input");
+        tokenInput.setAttribute('type', 'hidden');
+        tokenInput.setAttribute('name', 'token');
+        tokenInput.setAttribute('value', token);
+        form.appendChild(tokenInput);
+
+        form.submit();
+      };
+    });
+  });
+};
+
+window.addEventListener("turbo:load", pay);
+window.addEventListener("turbo:render", pay);
