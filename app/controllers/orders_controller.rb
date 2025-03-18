f2eb2a3867b5fb0289c263cd5f085eb9d5637511
peco-tanaka <ok.computeruk1929@gmail.com>
2025-03-18 13:24:35 +0900

    orderコントローラーの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,42 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
+  before_action :set_item, only: [:index, :create]
+  attr_accessor :token
+
   def index
+    @order_form = OrderForm.new
+  end
+
+  def create
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      pay_item
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_form).permit(
+    :token, :postal_code, :prefecture_id, :city, :address, :building, :phone_number
+    ).merge(
+    user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
