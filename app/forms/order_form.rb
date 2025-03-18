949ca92e6487baf7bc71043f1f016e6b278035c5
peco-tanaka <ok.computeruk1929@gmail.com>
2025-03-18 18:13:02 +0900

    tokenのバリデーションにエラーメッセージを追加

--- app/forms/order_form.rb
+++ app/forms/order_form.rb
@@ -5,15 +5,15 @@ class OrderForm
                 :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :token
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "invalid. Enter it as follows (e.g. 123-4567)" }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
-    validates :phone_number, format:{ with: /\A\d+\z/, message: "is invalid. Input only number" }, length: { minimum: 10, too_short: "is too short" }
+    validates :phone_number, format:{ with: /\A\d+\z/, message: "is invalid. Input only number" }, length: { minimum: 10, maximum: 11, too_short: "is too short", too_long: "is too long" }
     validates :user_id
     validates :item_id
   end
+  validates :token, presence: { message: "can't be blank" }
 
   def save
     ActiveRecord::Base.transaction do
