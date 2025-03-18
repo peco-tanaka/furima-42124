d3f6a899fd7113f73d9405262fd7288fa451a11a
peco-tanaka <ok.computeruk1929@gmail.com>
2025-03-18 12:24:26 +0900

    derivery_addressesテーブルに外部キーorder_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_16_033453) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_18_031901) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -48,6 +48,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_16_033453) do
     t.string "phone_number", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "order_id_id", null: false
+    t.bigint "order_id", null: false
+    t.index ["order_id"], name: "index_delivery_addresses_on_order_id"
+    t.index ["order_id_id"], name: "index_delivery_addresses_on_order_id_id"
   end
 
   create_table "items", charset: "utf8mb3", force: :cascade do |t|
@@ -94,6 +98,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_16_033453) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "delivery_addresses", "orders"
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
