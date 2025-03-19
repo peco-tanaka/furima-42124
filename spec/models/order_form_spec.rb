9feff42fdf18b9577aaefab0abe0eb281254cbf1
peco-tanaka <ok.computeruk1929@gmail.com>
2025-03-19 17:18:40 +0900

    不要なtoken属性を削除

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe OrderForm, type: :model do
   before do
     @user = FactoryBot.create(:user)
     @item = FactoryBot.create(:item)
-    @order_form = FactoryBot.build(:order_form, token: "dummy_test_token", user_id: @user.id, item_id: @item.id)
+    @order_form = FactoryBot.build(:order_form, user_id: @user.id, item_id: @item.id)
   end
 
   describe '商品購入機能' do
